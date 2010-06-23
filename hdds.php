2e3fac6ee8328b57d4f6fbae92aa81359b557f61
Chris West (Faux) <git@goeswhere.com>
2010-06-23 13:41:22 +0100

    table function

--- hdds.php
+++ hdds.php
@@ -8,7 +8,6 @@
  </head>
 <body>
 <p>Data sourced from <a href="http://www.overclockers.co.uk/">Overclockers</a>.</p>
-<table><tr><th>Size (GB)</th><th>Cheapest</th><th>Average</th><th>Cheapest per GB</th><th>Average per GB</th><th>Sample size</th></tr>
 <?
 $str = '';
 foreach (array(
@@ -21,13 +20,35 @@ foreach (array(
 	) as $sub)
 	$str.=file_get_contents("http://www.overclockers.co.uk/productlist.php?groupid=701&catid=14&subid={$sub}");
 
-preg_match_all('/<a href="showproduct.*?([0-9.]+)((?:T|G))B.*?<span class="incVat">\(&pound;(\d+\.\d+)/s', $str, $regs);
+table($str);
 
-$bits = array();
-foreach ($regs[1] as $ind => $numpart)
-	$bits[($regs[2][$ind] == 'T' ? 1024 : 1) * $numpart][] = $regs[3][$ind];
+function table($str) {
+	?>
+	<table><tr><th>Size (GB)</th><th>Cheapest</th><th>Average</th><th>Cheapest per GB</th><th>Average per GB</th><th>Sample size</th></tr>
+	<?
+	preg_match_all('/<a href="showproduct.*?([0-9.]+)((?:T|G))B.*?<span class="incVat">\(&pound;(\d+\.\d+)/s', $str, $regs);
 
-ksort($bits);
+	$bits = array();
+	foreach ($regs[1] as $ind => $numpart)
+		$bits[($regs[2][$ind] == 'T' ? 1024 : 1) * $numpart][] = $regs[3][$ind];
+
+	ksort($bits);
+	$bestavg = $bestsiz = 1337;
+
+	foreach ($bits as $size => $drives)
+		if (min($drives)/$size < $bestavg) {
+			$bestavg = min($drives)/$size;
+			$bestsiz = $size; 
+		}
+
+	foreach ($bits as $size => $drives)
+		echo "<tr" . ($size == $bestsiz ? ' class="hi"' : '') . ">" .
+			"<td>$size</td><td>" . price(min($drives)) . "</td>" .
+			"<td>" . price(average($drives)) . "</td>" .
+			"<td>" . price(min($drives)/$size, 3) . "</td>" .
+			"<td>" . price(average($drives)/$size,3) . "</td>" .
+			"<td>" . count($drives) . "</td></tr>\n";
+}
 
 function average(array $arr) {
 	return array_sum($arr) / count($arr);
@@ -37,22 +58,6 @@ function price($num, $level = 2) {
 	return number_format($num, $level);
 }
 
-$bestavg = $bestsiz = 1337;
-
-foreach ($bits as $size => $drives)
-	if (min($drives)/$size < $bestavg) {
-		$bestavg = min($drives)/$size;
-		$bestsiz = $size; 
-	}
-
-
-foreach ($bits as $size => $drives)
-	echo "<tr" . ($size == $bestsiz ? ' class="hi"' : '') . ">" .
-		"<td>$size</td><td>" . price(min($drives)) . "</td>" .
-		"<td>" . price(average($drives)) . "</td>" .
-		"<td>" . price(min($drives)/$size, 3) . "</td>" .
-		"<td>" . price(average($drives)/$size,3) . "</td>" .
-		"<td>" . count($drives) . "</td></tr>\n";
 ?>
 </table>
 </body>
